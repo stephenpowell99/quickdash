d62ba2355a3c2214fb2035ae59b28053b78c99a9
Steve <stephenpowell99@gmail.com>
2025-03-19 17:09:04 +0000

    vercel fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  outputFileTracingExcludes: {
+    '*': ['.next/trace']
+  }
 };
 
 export default nextConfig;
